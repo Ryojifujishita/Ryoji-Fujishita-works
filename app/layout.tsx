028cb4f3772013eaa8d0815f26a3851057451031
藤下了慈 <ryoji.fujishita@yappli.co.jp>
2025-05-02 17:14:38 +0900

    Remove unused Inter import from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { TinaWrapper } from "./tina-provider";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ryoji Portfolio",
